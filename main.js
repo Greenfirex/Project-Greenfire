786820afab83a614bc353684a36d3f8cf3dc4c01
Greenfirex <luky.losos@gmail.com>
2024-05-29 17:36:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
+    type: Phaser.CANVAS,
+    width: window.innerWidth, // Nastavuje šířku scény podle šířky okna prohlížeče
+    height: window.innerHeight, // Nastavuje výšku scény podle výšky okna prohlížeče
     scene: {
         preload: preload,
         create: create,
@@ -12,22 +12,34 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-	// Load assets here (images, sounds, etc.)
-	this.load.image('background', 'assets/images/background.jpg');
+    // Načtěte zde zdroje (obrázky, zvuky, atd.)
+    this.load.image('background', 'assets/images/background.jpg');
 }
 
 function create() {
-	// Initialize game objects and variables here
-	this.add.image(960, 540, 'background').setOrigin(0.5, 0.5);
-const graphics = this.add.graphics();
-    graphics.lineStyle(2, 0xffffff, 1);
+    // Inicializace herních objektů a proměnných zde
+    this.add.image(config.width / 2, config.height / 2, 'background').setDisplaySize(config.width, config.height); // Nastavíte obrázek pozadí tak, aby vyplňoval celou scénu
+
+    const graphics = this.add.graphics();
+    graphics.lineStyle(2, 0x00ff00, 1);
+
+// Vodorovná čára
     graphics.beginPath();
-    graphics.moveTo(1344, 0); // 1920 * 0.7 = 1344
-    graphics.lineTo(1344, 1080);
+    graphics.moveTo(0, config.height * 0.1); // Začátek na 10% od vrchu
+    graphics.lineTo(config.width * 0.8, config.height * 0.1); // Končí na svislé čáře
     graphics.strokePath();
+
+// Svislá čára
+    graphics.beginPath();
+    graphics.moveTo(config.width * 0.8, 0);
+    graphics.lineTo(config.width * 0.8, config.height);
+    graphics.strokePath();
+
+
+   
 }
 
 function update() {
-    // Game loop logic goes here
+    // Logika herní smyčky jde sem
 }
 
