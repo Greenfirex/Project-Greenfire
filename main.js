adc559c8f5065028af9d2ee4899286dc8de70f29
Greenfirex <luky.losos@gmail.com>
2024-05-29 16:44:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     scene: {
         preload: preload,
         create: create,
@@ -12,12 +12,13 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-    // Load assets here (images, sounds, etc.)
+	// Load assets here (images, sounds, etc.)
+	this.load.image('background', 'assets/images/background.jpg')
 }
 
 function create() {
-    // Initialize game objects and variables here
-    this.add.text(100, 100, 'Welcome to Sci-Fi Idle Game', { font: '24px Arial', fill: '#ffffff' });
+	// Initialize game objects and variables here
+	this.add.image(960, 540, 'background').setOrigin(0.5, 0.5);;
 }
 
 function update() {
