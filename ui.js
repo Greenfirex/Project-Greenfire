8a3fc823e52d8c4042b014cdfe28cb93e00c8396
Greenfirex <luky.losos@gmail.com>
2024-05-31 13:54:22 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,12 +1,14 @@
-export function createButton(scene, x, y, imageKey, text) {
+export function createButton(scene, x, y, imageKey, text, callback) {
     let buttonImage = scene.add.image(0, 0, imageKey);
     let buttonText = scene.add.text(0, 0, text, { fontSize: '20px', fill: '#ffffff' });
 
+    buttonText.setOrigin(0.5, 0.5);
+    buttonImage.setOrigin(0.5, 0.5);
+
     let buttonContainer = scene.add.container(x, y, [buttonImage, buttonText]);
 
-    buttonText.setOrigin(0.5, 0.5);
-    buttonText.x = buttonImage.width / 2;
-    buttonText.y = buttonImage.height / 2;
+    buttonText.x = 0;
+    buttonText.y = 0;
 
     buttonContainer.setSize(buttonImage.width, buttonImage.height);
     buttonContainer.setInteractive().on('pointerdown', callback);
