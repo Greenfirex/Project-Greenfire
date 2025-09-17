bf0099b6fab15b05b37ee9bf5f42a2da8fa43e5b
Lukas Losos <Lukas.Losos@warnermusic.com>
2025-09-17 14:34:38 +0200

    Create galaxyMap.js

---
+++ sections/galaxyMap.js
@@ -0,0 +1,5 @@
+// concepts
+export function setupGalaxyMapSection(galaxyMapSection) {
+    if (!galaxyMapSection) { return; }
+    galaxyMapSection.innerHTML = `<h2>Galaxy Map</h2><p>Feature coming soon...</p>`;
+}
