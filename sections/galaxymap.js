909ba9e7a470adb930bc8e961768c1776ce48dff
Lukas Losos <Lukas.Losos@warnermusic.com>
2025-09-17 14:33:17 +0200

    Delete galaxymap.js

--- sections/galaxymap.js
+++
@@ -1,5 +0,0 @@
-// concept
-export function setupGalaxyMapSection(galaxyMapSection) {
-    if (!galaxyMapSection) { return; }
-    galaxyMapSection.innerHTML = `<h2>Galaxy Map</h2><p>Feature coming soon...</p>`;
-}
