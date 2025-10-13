1bef561705c153fb2f793fb6b3459c2023f754a1
Lukas Losos <Lukas.Losos@warnermusic.com>
2025-10-13 11:59:47 +0200

    structure changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import { setupShipyardSection } from './sections/shipyard.js';
 import { setupGalaxyMapSection } from './sections/galaxyMap.js';
 import { loadGameState, saveGameState, resetToDefaultState } from './saveload.js';
 import { addLogEntry } from './log.js';
-import { showStoryPopup } from './data/popup.js';
+import { showStoryPopup } from './popup.js';
 import { storyEvents } from './data/storyEvents.js';
 import { initOptions, setGlowColor, setActiveGlowColor, setGlowIntensity } from './options.js';
 import { updateImpactTimer } from './eventManager.js';
